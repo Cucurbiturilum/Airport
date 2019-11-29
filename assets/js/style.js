7f255855a8c5c67da1cf6f830d9c4e012cbbd8e5
cucurbiturilum <cucurbiturilum@gmail.com>
2019-11-29 23:42:12 +0200

    add datetimepicker and change main table

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var arrivalBtn = $('#btn-arrival');
     var departureBtn = $('#btn-departure');
 
@@ -8,17 +8,32 @@ $(document).ready(function(){
     arrivalContent.show();
     departureContent.hide();
 
-    arrivalBtn.click(function(){
+    arrivalBtn.click(function () {
         departureBtn.removeClass('active');
         arrivalBtn.addClass('active');
         arrivalContent.show();
         departureContent.hide();
     });
 
-    departureBtn.click(function(){
+    departureBtn.click(function () {
         departureBtn.addClass('active');
         arrivalBtn.removeClass('active');
         arrivalContent.hide();
         departureContent.show();
+    });
+    $('#datetimepicker1').datetimepicker({
+        format: 'L'
+    });
+    $('#datetimepicker2').datetimepicker({
+        format: 'LT'
+    });
+    $('#datetimepicker3').datetimepicker({
+        format: 'L'
+    });
+    $('#datetimepicker4').datetimepicker({
+        format: 'LT'
+    });
+    $('#datetimepicker5').datetimepicker({
+        format: 'L'
     });
 });
